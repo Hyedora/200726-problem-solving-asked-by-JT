03c9619bacc90cb1a5a40fb5d307a13eaf3bedf8
Hyedora <byj4565@kaist.ac.kr>
2020-07-26 20:38:01 +0900

    first commit

---
+++ solve_Hyemin.py
@@ -0,0 +1,12 @@
+def initialize():
+    set_size = input("Insert the size of each number set : ")
+    max_gear_size = input("Insert the maximum size of the gear you want : ")
+    return set_size, max_gear_size
+
+
+
+def main():
+    set_size, max_gear_size = initialize()
+    
+
+main()
