e7177c76c08a9d4bcde47c1d84967022795a02d9
Hyedora <byj4565@kaist.ac.kr>
2020-07-26 21:29:50 +0900

    constructing data set

--- solve_Hyemin.py
+++ solve_Hyemin.py
@@ -3,12 +3,35 @@ def initialize():
     max_gear_size = input("Insert the maximum size of the gear you want : ")
     return set_size, max_gear_size
 
+def factorization(g):
+    if g<=0:
+        return [],[]
+    firsts=[]
+    seconds=[]
+    for i in range(g):
+        if g%i==0:
+            firsts.append(i)
+            seconds.append(g//i)
+    ################ continue with factorization(g-1) and factorization(g+1) #####
+
+    return firsts, seconds
 
 
 def main():
     set_size, max_gear_size = initialize()
-    num_pair=dict()
+    firsts=dict()
+    
     for g in range(1, max_gear_size+1):
-        num_pair[g]=
+        g_firsts, g_seconds = factorization(g)
+        for i in range(len(g_firsts)):
+            if not g_firsts[i] in firsts:
+                firsts[g_firsts[i]] = {g_seconds[i]:[g]}
+                continue
+            if not g_seconds[i] in firsts[g_firsts[i]]:
+                firsts[g_firsts[i]][g_seconds[i]] = [g]
+                continue
+            firsts[g_firsts[i]][g_seconds[i]].append(g)
+
+        
     
 main()
--- test.py
+++ test.py
@@ -1,2 +1,7 @@
-L=[1,2,[4,5],3]
-print(L.index(4))
+d = dict()
+
+if not 1 in d:
+    d[1]={2:[3]}    
+d[1][2].append(5)
+
+print(d)
