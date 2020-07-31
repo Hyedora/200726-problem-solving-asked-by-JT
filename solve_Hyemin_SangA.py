169d8a796516b3b6bf3830ef8454219615d0fa97
Hyedora <byj4565@kaist.ac.kr>
2020-07-31 16:44:39 +0900

    This is my limit. END

--- solve_Hyemin_SangA.py
+++ solve_Hyemin_SangA.py
@@ -25,9 +25,9 @@ test2 = test1.copy()
 
 # test 수들 중에 이미 set에 들어있는 수가 존재한다면 제거한다
 for i in test1:
-    if i in set1:
+    if i in set1 or i==0:
         test1.pop(test1.index(i))
-for i in test2:
+for i in test2 or i==0:
     if i in set2:
         test2.pop(test2.index(i))
 
@@ -36,24 +36,25 @@ set1_test = list(combinations(test1, SET_SIZE-len(set1)))
 set2_test = list(combinations(test2, SET_SIZE-len(set2)))
 
 # 위에서 만든 수 조합들을 모두 대입해보면서 해답이 존재하는지 찾는다
-solution_num=0
+solution=[]
 print()
 for test1 in set1_test:
     set1.extend(test1)
     for test2 in set2_test:
         set2.extend(test2)
-        # 문제 조건을 만족한다면 해당 set을 출력한다
+        # 문제 조건을 만족한다면 해당 set을 저장한다
         if(IsItRight(set1, set2)):
-            solution_num+=1
-            print("SOLUTION"+str(solution_num)+": ", end="")
-            print(sorted(set1), end=", ")
-            print(sorted(set2))
+            if not sorted([sorted(set1), sorted(set2)]) in solution:
+                solution.append(sorted([sorted(set1), sorted(set2)]))
         for i in range(len(test2)):
             set2.pop()
     for i in range(len(test1)):
         set1.pop()
 
-if solution_num==0:
+# 결과를 출력한다
+if len(solution)==0:
     print("가능한 solution 조합이 없습니다.")
 else:
-    print("%d개의 solution을 발견했습니다." %solution_num)
+    print("%d개의 solution을 발견했습니다." %len(solution))
+    for i in range(len(solution)):
+        print("solution %d:" %i, solution[i])
--- test.py
+++ test.py
@@ -1,3 +1,5 @@
-l=[1,3,5,2,3]
-print(l)
-print(sorted(l))
+l1=[1,2,3,2]
+l2=[5,6,7,3]
+sol=[]
+sol.append(sorted([sorted(l1), sorted(l2)]))
+print(sol)
